541173dab61c679dcff768c7e6c0570214a97c79
Vincent <b07209016@ntu.edu.tw>
2021-04-08 22:46:52 +0800

    good coding style script update

--- good_coding_style/ccs/solution2_axis/directives.tcl
+++ good_coding_style/ccs/solution2_axis/directives.tcl
@@ -3,3 +3,7 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
 ############################################################
+set_directive_top -name top "top"
+set_directive_interface -mode axis -register -register_mode both "top" in1
+set_directive_interface -mode axis -register -register_mode both "top" in2
+set_directive_interface -mode s_axilite "top"
--- good_coding_style/ccs/solution2_axis/script.tcl
+++ good_coding_style/ccs/solution2_axis/script.tcl
@@ -7,11 +7,11 @@ open_project good_coding_style
 set_top top
 add_files good_coding_style/src/good-stream.cpp
 add_files good_coding_style/src/good-stream.hpp
-add_files -tb good_coding_style/src/good-test.cpp
-open_solution "solution1"
-set_part {xc7z020clg400-1}
+add_files -tb good_coding_style/src/good-test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+open_solution "solution2_axis"
+set_part {xc7z020-clg400-1}
 create_clock -period 5 -name default
-#source "./good_coding_style/solution1/directives.tcl"
+source "./good_coding_style/ccs/solution2_axis/directives.tcl"
 csim_design
 csynth_design
 cosim_design -trace_level all -tool xsim
